47050a78dbb2f94859611fe7149567de4b3baabd
gurung.sa <saurabhgurung20@gmail.com>
2025-01-01 08:10:26 -0800

    completed the login part

---
+++ Pennywise/Components/Helpers/UserStorage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using Pennywise.Components.Models;
+
+namespace Pennywise.Components.Helpers
+{
+    public static class UserStorage
+    {
+        private static string FilePath = Path.Combine(AppContext.BaseDirectory, "Components", "Data", "user.json");
+
+        public static User LoadUser()
+        {
+            if (File.Exists(FilePath))
+            {
+                var json = File.ReadAllText(FilePath);
+
+                return JsonSerializer.Deserialize<User>(json);
+            }
+            return null;
+        }
+    }
+}
