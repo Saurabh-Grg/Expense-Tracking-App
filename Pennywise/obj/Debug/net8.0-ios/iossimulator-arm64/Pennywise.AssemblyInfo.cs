66d5213047f4042a1cab8d4c450f219357171f8c
gurung.sa <saurabhgurung20@gmail.com>
2025-01-03 08:40:56 -0800

    updated code for settings' UI

--- Pennywise/obj/Debug/net8.0-ios/iossimulator-arm64/Pennywise.AssemblyInfo.cs
+++ Pennywise/obj/Debug/net8.0-ios/iossimulator-arm64/Pennywise.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pennywise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+47050a78dbb2f94859611fe7149567de4b3baabd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+ee6654794fdc9f050380aa734b134d504836498b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pennywise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pennywise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Pennywise/obj/Debug/net8.0-windows10.0.19041.0/win10-arm64/Pennywise.AssemblyInfo.cs
+++ Pennywise/obj/Debug/net8.0-windows10.0.19041.0/win10-arm64/Pennywise.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pennywise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+63caeffd8e4bd0f3db3233728a9585290e6fd64a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+ee6654794fdc9f050380aa734b134d504836498b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pennywise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pennywise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
